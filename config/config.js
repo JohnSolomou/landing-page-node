451f03b88b78a9bf2a1a99a138c3965f1096d0e3
John Solomou <solomoufam@yahoo.com>
2022-06-11 08:48:22 -0400

    created and connected to database

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: "postgres",
+    password: process.env.PG_PASSWORD,
+    database: "express-mbp-db",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    port: 5432,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
--- controllers/landing.js
+++ controllers/landing.js
@@ -1,4 +1,9 @@
 exports.get_landing = function (req, res, next) {
   res.render("landing", { title: "Express" });
 };
+exports.submit_lead = function (req, res, next) {
+  console.log("lead email", req.body.lead_email);
+  res.redirect("/");
+};
+
 //controllers allow us to separate handler logic from route handler logic
